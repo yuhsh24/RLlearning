2a49ac9ea877cae27c0ce513b795c10a2266b166
yuhsh24 <yhshzju@163.com>
2016-07-21 17:48:49 +0800

    third submit
    QLearning

--- CartpoleControl/experiment.py
+++ CartpoleControl/experiment.py
@@ -15,14 +15,14 @@ if __name__ == "__main__":
                        alpha = 0.1, \
                        gamma = 0.9)
 
-    cartpole.monitor.start('./cartpole-experiment-1',force=True)
+    #cartpole.monitor.start('./cartpole-experiment-1',force=True)
     for iter1 in xrange(20):
         s_f = cartpole.reset()
         for iter2 in xrange(2000):
-            cartpole.render()
+            #cartpole.render()
             a = policy.greedy(s_f)
             s_f, r, t, i = cartpole.step(a)
             if t:
                 print "Episode finished after {} timesteps".format(iter2+1)
                 break
-    cartpole.monito.close()
+    #cartpole.monitor.close()
